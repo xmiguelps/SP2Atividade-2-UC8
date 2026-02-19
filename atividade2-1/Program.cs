f9c60a9ab98ac9c587233b2653453bb4a989d1f5
Kauan Gaspar Dias <kauanq.dias@gmail.com>
2026-02-18 21:49:41 -0300

    salvo

---
+++ atividade2-1/Program.cs
@@ -0,0 +1,29 @@
+﻿int i = 1;
+int pares = 0;
+int impars = 0;
+
+int[] numeros = new int[6];
+
+Console.WriteLine("Insira 5 numeros");
+while (i <= 5)
+{
+    Console.Write($"{i}° Numero: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+    if (numeros[i] == 0)
+    {
+        Console.WriteLine("0 não pode ser digitado");
+        break;
+    }
+    else if (numeros[i] % 2 == 0)
+    {
+        pares++;
+    }
+    else
+    {
+        impars++;
+    }
+    i++;
+}
+
+Console.WriteLine($"A quantidade de numeros pares foi {pares}");
+Console.WriteLine($"A quantidade de numeros impares foi {impars}");
---
+++ atividade2-2/Program.cs
@@ -0,0 +1,17 @@
+﻿Random random = new Random();
+int num = 0;
+int random_num = random.Next(1, 101);
+Console.WriteLine("Acerte o numero entre 1 e 100!!!");
+do
+{
+    Console.Write("Digite o numero: ");
+    num = int.Parse(Console.ReadLine());
+    if (num != random_num)
+    {
+        Console.WriteLine("Errou, tente novamente");
+    } else
+    {
+        Console.WriteLine("Parabens, voce acertou");
+        break;
+    }
+} while (num != random_num);
